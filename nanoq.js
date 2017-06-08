78604363bad498c10833523f96206be81a25495d
Alan MacLeod <amacleod@rskorbital.com>
2017-06-08 21:56:14 +0800

    fix <= bug

--- nanoq.js
+++ nanoq.js
@@ -26,16 +26,16 @@ export default class nanoq
 
   sink(i)
   {
-    let i2, i21, m;
-    while(i << 1 <= this.p)
+    let a, b, c;
+    while(i << 1 < this.p)
     {
-      i2 = i << 1; i21 = i2++;
-      m = (i21 > this.p) ? i2 : this.c(i21, i2) ? i2 : i21; // nb: .c(b,a)
+      a = i << 1; b = a++;
+      c = (b > this.p) ? a : this.c(b, a) ? a : b;
 
-      if (this.c(i, m))
-        [this.data[i], this.data[m]] = [this.data[m], this.data[i]];
+      if (this.c(i, c))
+        [this.data[i], this.data[c]] = [this.data[c], this.data[i]];
       else return;
-      i = m;
+      i = c;
     }
   }
 
