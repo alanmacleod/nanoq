d19a9ff17dbbdee7b7bc50846546baef127eae58
Alan MacLeod <amacleod@rskorbital.com>
2017-06-09 11:53:15 +0800

    smllr

--- nanoq.js
+++ nanoq.js
@@ -1,35 +1,30 @@
-
 export default class nanoq
 {
   constructor(max, compare)
   {
-    this.tree = !max ? [] : (max < 65536 ? (max < 256 ? new Uint8Array(max) :
-                new Uint16Array(max)) : new Uint32Array(max));
-    this.cmp = compare || ((a, b) => this.tree[a] > this.tree[b]);
+    this.tree = !max ? [] : (max < 65536 ? (max < 256 ? new Uint8Array(max)  :
+                             new Uint16Array(max))    : new Uint32Array(max));
+    this.cmp = compare || ((a, b) => this.tree[a] < this.tree[b]);
     this.peek = () => { return this.tree[1]; }
   }
 
   push(n)
   {
-    if (this.p) this.tree[this.p++] = n;
-    else this.tree[(this.p = 2) - 1] = n;
-    this.swim();
+    if (this.p) (this.tree[this.p++] = n) && this.swim();
+    else        this.tree[(this.p = 2) - 1] = n;
   }
 
   pop()
   {
     if (this.p == 1 || !this.p) return null;
     let r = this.tree[1];
-    this.tree[1] = this.tree[(this.p--) - 1];
-    this.sink(1);
-    return r;
+    return ((this.tree[1] = this.tree[(this.p--) - 1]) && this.sink(1)) || r;
   }
 
   sink(i)
   {
     let j, k, c;
-    while((j = i<<1) < this.p)
-    {
+    while((j = i<<1) < this.p) {
       k = j + 1;
       c = (k > this.p) ? j : this.cmp(k, j) ? j : k;
       if (this.cmp(i, c))
@@ -42,8 +37,7 @@ export default class nanoq
   swim()
   {
     let p = this.p - 1, q = p >> 1;
-    while (q && this.cmp(q, p))
-    {
+    while (q && this.cmp(q, p)) {
       [this.tree[q], this.tree[p]] = [this.tree[p], this.tree[q]];
       q = (p >>= 1) >> 1;
     }
